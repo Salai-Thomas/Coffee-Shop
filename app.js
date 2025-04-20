ef10b5b289bf7a3986fdcc6e71b04f0352c05a44
SalaiThomas <salaithomas582@gmail.com>
2025-04-20 10:17:33 +0630

    add orderline clone with jquery

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+$(document).ready(function(){
 // Home Page
 $('.btn-close').click(function(){
   $('#myModal').removeClass("show");
 });
+
+//Cart Page
+
+let orderlines = [
+  {"product":"producta","price":20,"qty":2},
+  {"product":"productb","price":20,"qty":3},
+  {"product":"productc","price":30,"qty":4},
+];
+
+orderlines.forEach(function(index,line){
+
+  $('#orderlines').append(
+    `<tr id="item${line}">
+    <td>${line.product}</td>
+    <td>${line.price}</td>
+    <td class="text-center">${line.qty}</td>
+    <td class="text-end">${line.qty *line.price}</td>
+    </tr>`
+  );
+});
+
+});
+
+
 
 
