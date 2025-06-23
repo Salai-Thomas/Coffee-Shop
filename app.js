3c3fd359f0fd1a5e2fb02866732d78efb6f9b7ea
SalaithomasBee <salaithomas@beedatamyanmar.com>
2025-06-23 09:28:31 +0630

    updated all

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
+// import Typed from 'typed.js';
+
+//     const input = document.getElementById("search-input");
+//     const placeholder = document.querySelector(".typed-placeholder");
+
+//     // Hide animation when user types
+//     input.addEventListener("#search-input", () => {
+//       placeholder.style.display = input.value ? "none" : "block";
+//     });
+
+//     // Typed.js init
+//     var typed = new Typed("#typed", {
+//       strings: ["Coffee", "Snacks", "Desserts", "Cold Brew", "Croissants"],
+//       typeSpeed: 70,
+//       backSpeed: 40,
+//       backDelay: 1500,
+//       loop: true
+//     });
 $(document).ready(function(){
 // Home Page
 $('.btn-close').click(function(){
   $('#myModal').removeClass("show");
 });
 
+
+
 //Cart Page
 
 let orderlines = [
-  {"product":"producta","price":20,"qty":2},
-  {"product":"productb","price":20,"qty":3},
-  {"product":"productc","price":30,"qty":4},
+  {"product":"Cappuccino","price":20,"qty":2},
+  {"product":"Choco Cappuccino","price":20,"qty":3},
+  {"product":"Choco Coffee","price":30,"qty":4},
 ];
 
-orderlines.forEach(function(index,line){
+orderlines.forEach(function(line,index){
 
   $('#orderlines').append(
-    `<tr id="item${line}">
+    `<tr id="item${index}">
     <td>${line.product}</td>
     <td>${line.price}</td>
-    <td class="text-center">${line.qty}</td>
+    <td class="text-center"><a class="btn "><i class="bi bi-plus-circle"></i></a>${line.qty}<a class="btn"><i class="bi bi-dash-circle"></i></a></td>
     <td class="text-end">${line.qty *line.price}</td>
     </tr>`
   );
+
 });
+
+
 
 });
 
