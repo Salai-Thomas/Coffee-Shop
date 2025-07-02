b65ac45c9dff254e8d247c34c2aabfe2f1c4c8df
SalaiThomasBee <salaithomas@beedatamyanmar.com>
2025-07-02 16:30:19 +0630

    increment decrement function

--- Cart/cart.js
+++ Cart/cart.js
@@ -5,19 +5,46 @@ let orderlines = [
   {"product":"Choco Coffee","price":30,"qty":4},
 ];
 
+  function updateRowUI(index) {
+    let row = $(`#item${index}`);
+    row.find('.qty').text(orderlines[index].qty);
+    row.find('.total').text(orderlines[index].qty * orderlines[index].price);
+  }
+
 orderlines.forEach(function(line,index){
 
   $('#orderlines').append(
     `<tr id="item${index}">
     <td>${line.product}</td>
     <td>${line.price}</td>
-    <td class="text-center"><a class="btn "><i class="bi bi-plus-circle"></i></a>${line.qty}<a class="btn"><i class="bi bi-dash-circle"></i></a></td>
-    <td class="text-end">${line.qty * line.price}</td>
+    <td class="text-center"><a class="btn increment" data-index="${index}"><i class="bi bi-plus-circle"></i></a><span class='qty'>${line.qty}</span><a class="btn decrement" data-index="${index}"><i class="bi bi-dash-circle"></i></a></td>
+    <td class="text-end"><span class='total'>${line.qty * line.price}</span></td>
     </tr>`
   );
 
 });
 
 
+  $(document).on('click', '.increment', function() {
+    let index = $(this).data('index');
+    console.log("Increment clicked for item index:", index);
+    orderlines[index].qty += 1;
+    console.log(orderlines[index].qty);
+    updateRowUI(index);
+  });
+
+  $(document).on('click', '.decrement', function() {
+    let index = $(this).data('index');
+    console.log("Increment clicked for item index:", index);
+    if(orderlines[index].qty != 0){
+      orderlines[index].qty -= 1;
+      updateRowUI(index);
+    }
+
+  });
+
 
 });
+
+
+
