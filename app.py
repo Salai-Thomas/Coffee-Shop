89d24e230a1c0802f64a10a6775ff24e274e1223
SalaiThomas <salaithomas582@gmail.com>
2025-07-05 16:14:36 +0630

    all left page changed

--- app.py
+++ app.py
@@ -10,5 +10,20 @@ def home():
 def coffee():
     return render_template('coffee.html')
 
+@app.route('/equip')
+def equip():
+    return render_template('equip.html')
+
+@app.route('/event')
+def event():
+    return render_template('event.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/subscription')
+def subscription():
+    return render_template('subscription.html')
 if __name__ == '__main__':
     app.run(debug=True)
